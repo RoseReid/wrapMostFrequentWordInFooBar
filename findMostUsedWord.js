6df90fd7f4c0eaa294bae13ea34cdb00961fa475
RoseReid <rosemaryalicereid@gmail.com>
2017-01-24 11:02:00 +0100

    second solution

--- findMostUsedWord.js
+++ findMostUsedWord.js
@@ -1,13 +1,15 @@
 module.exports = function(text){
 
-var words= text.split(/[\s.:><,]+/);
-var mf = 1; //default maximum frequency
-var m = 0;  //counter
-var item;  //to store item with maximum frequency
-var replaceText; //array
+var wordCounts = { };
+var mf = 1;
+var m = 0;
+var words = text.toLowerCase().split(/[\s.:><,]+/);
+for(var i = 0; i < words.length; i++)
+( wordCounts[ words[i]] = (wordCounts[words[i]] || 0) + 1)
+
 var addFooBar = function(){
-     replaceText = ("foo"+ item +"bar");
-     var newRegEx= new RegExp('\\b' +item + '\\b', 'g')
+     replaceText = ("foo"+ wordCounts +"bar");
+     var newRegEx= new RegExp('\\b' +wordCounts + '\\b', 'g')
     return text.replace(newRegEx, replaceText)
        //console.log(words.splice(words,1,replaceText))
        
@@ -19,13 +21,15 @@ for (var i=0; i<words.length; i++)    //select element (current element)
         {
                 if (words[i] == words[j])     //see if element occurs again in the array
                  m++;   //increment counter if it does
-                if (mf<m)   //compare current items frequency with maximum frequency
+                if (mf<m)   //compare current wordCountss frequency with maximum frequency
                 {
                   mf=m;      //if m>mf store m in mf for upcoming elements
-                  item = words[i];   // store the current element.
-                }
-        }
-        m=0;  
+                  wordCounts = words[i]; 
+                }   
+
+            }
+        m=0; 
+        
 }
 
 
