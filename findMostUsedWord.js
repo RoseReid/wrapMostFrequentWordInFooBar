8b7b5d1ddaf9723859e8563ea10ccf0cd8a22054
RoseReid <rosemaryalicereid@gmail.com>
2017-01-25 13:39:12 +0100

    changes

--- findMostUsedWord.js
+++ findMostUsedWord.js
@@ -3,45 +3,39 @@ module.exports = function(text){
 var wordCounts = { };
 var mf = 1;
 var m = 0;
+//var mostCommonWords = [];
 var words = text.toLowerCase().split(/[\s.:><,]+/);
-for(var i = 0; i < words.length; i++)
-( wordCounts[ words[i]] = (wordCounts[words[i]] || 0) + 1)  //look for array with wordcounts and take the heighest?
 
+for(var i = 0; i < words.length; i++){
+( wordCounts[ words[i]] = (wordCounts[words[i]] || 0) + 1)  //look for array with wordcounts and take the heighest?
 console.log(wordCounts)
 
 var addFooBar = function(){
      replaceText = ("foo"+ wordCounts +"bar");
-     var newRegEx= new RegExp('\\b' +wordCounts + '\\b', 'g')
+     var newRegEx= new RegExp('\\b' + wordCounts + '\\b', 'g')
     return text.replace(newRegEx, replaceText)
        //console.log(words.splice(words,1,replaceText))
        
-};
-
-for (var i=0; i<words.length; i++)    //select element (current element)
-{
-   var holder = function()  
-   {   
-       for (var j=i; j<words.length; j++)   //loop through next elements in array to compare calculate frequency of current element
+}
+for (var j=i; j<words.length; j++)   //loop through next elements in array to compare calculate frequency of current element
         {
-                if (words[i] == words[j])     //see if element occurs again in the array
+                if (words[i] === words[j])     //see if element occurs again in the array
                  m++;   //increment counter if it does
-                if (mf<m)   //compare current wordCountss frequency with maximum frequency
+                  
+            }
+            if (mf<m)   //compare current wordCountss frequency with maximum frequency
                 {
                   mf=m;      //if m>mf store m in mf for upcoming elements
-                  wordCounts = words[i]; 
-                }   
+                  wordCounts = words[i];
+                }
                 
-            }
-   m=0; 
+        
+   m=0;
 
-    }
 
-holder()   //I want to be able to call the function while wordcount of longest word is same as other words.
-   
+
 }
-
-
-
+       
  return addFooBar()
 }
 
