1f2561fb1722994cd13ec4ad6d45a04acb103961
RoseReid <rosemaryalicereid@gmail.com>
2017-01-29 17:54:39 +0100

    solved

--- findMostUsedWord.js
+++ findMostUsedWord.js
@@ -3,14 +3,12 @@ module.exports = function(text){
 var words= text.split(/[\s.:><,]+/);
 var mf = 1; //default maximum frequency
 var m = 0;  //counter
-var item;  //to store item with maximum frequency
+var items = [];  //to store item with maximum frequency
 var replaceText; //array
-var addFooBar = function(){
+var addFooBar = function(item){
      replaceText = ("foo"+ item +"bar");
      var newRegEx= new RegExp('\\b' +item + '\\b', 'g')
     return text.replace(newRegEx, replaceText)
-       //console.log(words.splice(words,1,replaceText))
-       
 };
 
 for (var i=0; i<words.length; i++)    //select element (current element)
@@ -19,17 +17,26 @@ for (var i=0; i<words.length; i++)    //select element (current element)
         {
                 if (words[i] == words[j])     //see if element occurs again in the array
                  m++;   //increment counter if it does
-                if (mf<m)   //compare current items frequency with maximum frequency
-                {
-                  mf=m;      //if m>mf store m in mf for upcoming elements
-                  item = words[i];   // store the current element.
-                }
+        }
+        if (mf<m)   //compare current items frequency with maximum frequency
+        {
+                mf=m;      //if m>mf store m in mf for upcoming elements
+                items = [];
+                items.push(words[i]);  //to save you do arrayname.push(item)
+                // store the current element.
+        } else if (mf === m) {
+                //handle tie here
+                items.push(words[i]);             
+                        console.log(items)
+
+                // newitem2 = words[i]+ 
         }
         m=0;  
 }
 
-
-
- return addFooBar()
+items.forEach(function(item){
+      text = addFooBar(item)
+})
+ return text;
 }
 
