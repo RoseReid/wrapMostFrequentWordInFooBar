b870a71ec5f6f39f5d5dabd3aad93a82645b71fd
RoseReid <rosemaryalicereid@gmail.com>
2017-01-31 21:25:46 +0100

    added gitignore

--- findMostUsedWord.js
+++ findMostUsedWord.js
@@ -1,41 +1,36 @@
 module.exports = function(text){
 
-var wordCounts = { };
-var mf = 1;
+var wordCounts = {};
+var mostFrequentWords = []
+var mf = 0;
 var m = 0;
 //var mostCommonWords = [];
 var words = text.toLowerCase().split(/[\s.:><,]+/);
 
 for(var i = 0; i < words.length; i++){
-( wordCounts[ words[i]] = (wordCounts[words[i]] || 0) + 1)  //look for array with wordcounts and take the heighest?
-console.log(wordCounts)
+    ( wordCounts[ words[i]] = (wordCounts[words[i]] || 0) + 1)  //look for array with wordcounts and take the heighest?
+    m = wordCounts[words[i]];
+    if (mf<m){
+        mostFrequentWords = []
+        mf=m;     
+        mostFrequentWords.push(words[i]);
+    } else if (mf === m){
+        mostFrequentWords.push(words[i]);
+    }
 
-var addFooBar = function(){
-     replaceText = ("foo"+ wordCounts +"bar");
-     var newRegEx= new RegExp('\\b' + wordCounts + '\\b', 'g')
+}
+
+var addFooBar = function(word){
+     var replaceText = ("foo"+ word +"bar");
+     var newRegEx= new RegExp('\\b' + word + '\\b', 'g')
     return text.replace(newRegEx, replaceText)
-       //console.log(words.splice(words,1,replaceText))
-       
 }
-for (var j=i; j<words.length; j++)   //loop through next elements in array to compare calculate frequency of current element
-        {
-                if (words[i] === words[j])     //see if element occurs again in the array
-                 m++;   //increment counter if it does
-                  
-            }
-            if (mf<m)   //compare current wordCountss frequency with maximum frequency
-                {
-                  mf=m;      //if m>mf store m in mf for upcoming elements
-                  wordCounts = words[i];
-                }
-                
-        
-   m=0;
+ console.log(mostFrequentWords) 
 
+ for (let j=0; j<mostFrequentWords.length; j++){
+     text = addFooBar(mostFrequentWords[j])
+ }  
 
-
-}
-       
- return addFooBar()
+ return text
 }
 
